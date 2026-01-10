4be1f10fc9b4a23228f89d0351f14b04ba45aa28
Brian Hodge <brian.hodgebh@googlemail.com>
2026-01-10 23:19:39 +0000

    added basic socket.io server

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", () => {
+  console.log(`user connected ${socket.id}`);
+
+  // Drawing logic goes here
+
+  socket.on('disconnected', () => {
+    cosole.log(`user disconnected ${socket.id}`)
+  });
+
+});
+
+server.listen(3001, () => {
+  console.log("SERVER RUNNING ON PORT 3001");
+});
